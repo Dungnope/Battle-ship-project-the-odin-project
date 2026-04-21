aec33815bc8422d17be9336b9a14a9a765e4abf3
unknown <140576667+Dungnope@users.noreply.github.com>
2026-04-21 16:13:53 +0700

    place boat function initial

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { Ship } from "./ship.js";
 import { createBoard, interactBoard } from "./UI components/board.js";
 
 let playerBoard = new Player("British", new Gameboard(10, 10));
-let playerBoard2 = new Player("British", new Gameboard(10, 10));
+let playerBoard2 = new Player("British34", new Gameboard(10, 10));
 
 createBoard(playerBoard);
 createBoard(playerBoard2);
+
+interactBoard(playerBoard);
+interactBoard(playerBoard2);
