80148b8b1164a57bec9f3dea568706f6a739da1a
unknown <140576667+Dungnope@users.noreply.github.com>
2026-05-03 20:53:59 +0700

    add initial play turn by turn with bot

--- src/UI components/botplay.js
+++ src/UI components/botplay.js
@@ -1,7 +1,24 @@
 export const botPlayGame = (player, bot) => {
   let turn = player.constructor.name;
-
+  let hasClicked = false;
   const playerBoard = document.getElementById(`${player.id}`);
   const botBoard = document.getElementById(`${bot.id}`);
   console.log(playerBoard, botBoard);
+
+  const botField = botBoard.querySelectorAll(".wrapper__grid .box");
+  botField.forEach((box) => {
+    box.addEventListener("click", (e) => {
+      if (turn === "Player" && !hasClicked) {
+        console.log("you attack");
+        hasClicked = !hasClicked;
+        turn = "Bot";
+      } else if (turn === "Bot" && hasClicked) {
+        hasClicked = !hasClicked;
+        setTimeout(() => {
+          console.log("Bot attack");
+          turn = "Player";
+        }, 1000);
+      }
+    });
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+// import "./style.css";
 import { Bot, Player } from "./player.js";
 import { Gameboard } from "./gameboard.js";
 import {
@@ -9,9 +9,7 @@ import {
 
 document.addEventListener("DOMContentLoaded", (e) => {
   let player1 = new Player("Parker", new Gameboard(10, 10));
-  let bot1 = new Bot("Parker", new Gameboard(10, 10));
   createBoard(player1, "var(--player-background)", true);
   chooseShip(player1);
   interactWithBoard(player1);
-  console.log(bot1.constructor.name);
 });
