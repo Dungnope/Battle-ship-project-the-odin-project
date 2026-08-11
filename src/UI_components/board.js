85d2f2b632c215cb5282fe8b6afbd77f0d2d432f
unknown <140576667+Dungnope@users.noreply.github.com>
2026-08-11 17:34:53 +0700

    delete drag and drop console.log

--- src/UI_components/board.js
+++ src/UI_components/board.js
@@ -275,7 +275,6 @@ const takeShipFromList = (eventTarget, eventType, player, axis) => {
 //hover with mark the ship size
 const shipHover = (shipLength, boxes, currentPos, status, gameboard, axis) => {
   let pathTracker = []; //tracker by condition
-  console.log("hover");
   for (let i = 0; i < shipLength; i++) {
     //check valid horizontal
     if (currentPos.y + i < gameboard.column && axis === "horizontal") {
@@ -539,7 +538,6 @@ const chooseShip = (player) => {
 
       //end drag phase
       ship.addEventListener("dragend", (e) => {
-        console.log("end");
         if (!e.currentTarget.classList.contains("had__placed")) {
           e.currentTarget.classList.toggle("selected__ship");
         }
@@ -609,13 +607,11 @@ const interactWithBoard = function(player, mode){
 
       //use for target attachment item when an draggable item collision this element
       box.addEventListener("dragenter", (e) => {
-        console.log("enter grid");
         takeShipFromList(e.currentTarget, e.type, player, axis[current]);
         e.stopImmediatePropagation();
       });
 
       box.addEventListener("dragleave", (e) => {
-        console.log("leave grid");
         takeShipFromList(e.currentTarget, e.type, player, axis[current]);
         e.stopImmediatePropagation();
       });
@@ -628,7 +624,6 @@ const interactWithBoard = function(player, mode){
       box.addEventListener("drop", (e) => {
         takeShipFromList(e.currentTarget, e.type, player, axis[current]);
         placeShipOnBoard(player, e, current);
-        console.log("drop");
         if (isAllPlace(player)) {
           if(mode === "2player"){
             singlePlay.call({singleplayList: this.interactList}, player, mode);
